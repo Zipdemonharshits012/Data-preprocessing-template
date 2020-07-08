35771f6dd0a282dfde3d4b19de7128857290d462
Zipdemonharshits012 <64593254+Zipdemonharshits012@users.noreply.github.com>
2020-07-08 22:19:11 +0530

    Update Data Preprocessing Template.py

--- Data Preprocessing Template.py
+++ Data Preprocessing Template.py
@@ -23,21 +23,43 @@ print(y,"\n\n ")
 
 
 
-"""Missing data """
+'''"""Missing data """
 from sklearn.impute import SimpleImputer
 imputer=SimpleImputer(missing_values=np.nan,strategy="mean")
 x[:,1:3]=imputer.fit_transform(x[:,1:3])
 #y=imputer.fit_transform(y)
-print(x,"\n \n")
+print(x,"\n \n")'''
 
 
 
 
-"""Handling Categorical Data and Encoding it"""
-from sklearn.preprocessing import LabelEncoder , OneHotEncoder
-le=LabelEncoder()
-x[:,0]=le.fit_transform(x[:,0])
-y[:,0]=le.fit_transform(y[:,0])
+'''"""Handling Categorical Data and Encoding it"""
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [0])], remainder='passthrough')
+x= np.array(ct.fit_transform(x))'''
+
+'''Encoding the Dependent Variable"""
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y = le.fit_transform(y)'''
+
+
+
+
+'''#Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 1)
+'''
+
+
+
+
+'''#Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+x_train[:, 3:] = sc.fit_transform(x_train[:, 3:])
+x_test[:, 3:] = sc.transform(x_test[:, 3:])'''
 
 
 
